53cddeb9dfd6759a37b3f8c624d860cf51d1165a
jeremy <jezzacooks.com>
2026-02-24 21:59:00 +0100

    fix: change masthead edition from Flint to Kamp

--- src/components/courant/CourantMasthead.tsx
+++ src/components/courant/CourantMasthead.tsx
@@ -42,7 +42,7 @@ export function CourantMasthead() {
                         {dateStr}
                     </time>
                     <span className="hidden sm:inline">·</span>
-                    <span>Editie: Flint</span>
+                    <span>Editie: Kamp</span>
                     <span className="hidden sm:inline">·</span>
                     <span>Kamp 8, Amersfoort</span>
                 </div>
